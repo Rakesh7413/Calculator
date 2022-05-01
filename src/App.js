f0b52a88504522949ac8bd89d42a0c5d0221647c
Rakesh7413 <rakesh.munnangi.3@gmail.com>
2022-05-01 10:58:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cal, setCal] = useState("");
+    const [res, setRes] = useState("");
+
+    const op=['/', '*', '+', '-', '.'];
+
+    const updateCal = value => {
+        if(
+            op.includes(value) && cal === '' ||
+            op.includes(value) && op.includes(cal.slice(-1)
+            )
+        ) {
+            return;
+        }
+        setCal(cal +value);
+        if(!op.includes(value))
+        {
+            setRes(eval(cal+value).toString());
+        }
+    }
+    const digits  = () =>{
+        const digit = [];
+        for(let i=1;i<10;i++)
+        {
+            digit.push(<button onClick={() => updateCal(i.toString())} key = {i}>{i}</button>)
+        }
+        return digit;
+    }
+    const calculate = () => {
+        setCal(eval(cal).toString());
+    }
+    const deleteLast = () => {
+        if(cal == '')
+        {
+            return;
+        }
+        const value = cal.slice(0,-1);
+        setCal(value);
+    }
+    return ( 
+        <div className = "App">
+            <div className="claculator">
+                <div className="result">
+                    {res ? <span>({res})</span> : ''} {cal || "0"}
+                </div>
+                <div className="operators">
+                    <button onClick={() => updateCal('*')}>*</button>
+                    <button onClick={() => updateCal('/')}>/</button>
+                    <button onClick={() => updateCal('+')}>+</button>
+                    <button onClick={() => updateCal('-')}>-</button>
+                    <button onClick={deleteLast}>C</button>
+                </div>
+                <div className="numbers">
+                    { digits()}
+                    <button onClick={() => updateCal('0')}>0</button>
+                    <button onClick={() => updateCal('.')}>.</button>
+                    <button onClick={calculate}>=</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
